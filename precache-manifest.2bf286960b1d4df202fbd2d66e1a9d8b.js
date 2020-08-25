f57548e06d4d03bd3d4c9f5b53fa681e4275a415
avilego <menavirdgz@gmail.com>
2020-08-25 17:52:03 -0500

    Updates

---
+++ precache-manifest.2bf286960b1d4df202fbd2d66e1a9d8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47c9acba5cf1b91c63353ad35cd7a1a",
+    "url": "/buscadorPixabay/index.html"
+  },
+  {
+    "revision": "596b44a9c5dd407d38e1",
+    "url": "/buscadorPixabay/static/js/2.57d3c449.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buscadorPixabay/static/js/2.57d3c449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5e5070f38a1622357d",
+    "url": "/buscadorPixabay/static/js/main.64d93121.chunk.js"
+  },
+  {
+    "revision": "2f9fbffee7ff1ceaaf55",
+    "url": "/buscadorPixabay/static/js/runtime-main.ab07559b.js"
+  }
+]);
